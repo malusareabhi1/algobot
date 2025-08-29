f5981d33081f33da94d39c99c753fa4a68d84be6
Abhi Malusare <malusareabhi1@gmail.com>
2025-08-29 12:10:36 +0530

    Update project_algo.py

--- project_algo.py
+++ project_algo.py
@@ -7148,8 +7148,8 @@ elif selected == "3PM OPTION":
             'Close_^NSEI': 'Close'
         }, inplace=True)
         
-        st.write(f"All 15-min candles for today ({today_date}):")
-        st.table(display_df)
+        #st.write(f"All 15-min candles for today ({today_date}):")
+        #st.table(display_df)
     
     ###########################################################################################
     import pandas as pd

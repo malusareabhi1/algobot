1cb5f2d1764d65af9453c081cfbe88817feced2d
Abhi Malusare <malusareabhi1@gmail.com>
2025-12-08 11:46:32 +0530

    Update shree_algo.py

--- shree_algo.py
+++ shree_algo.py
@@ -5606,7 +5606,7 @@ elif MENU =="Live Trade":
         #calling all condition in one function
         # --- HARD BLOCK: do not detect any signal before 9:30 AM ---
         last_time = df["Datetime"].iloc[-1].time()
-        st.write("Last Time",last_time)
+        st.write("Last Time-",last_time)
         import datetime
         if last_time < datetime.time(9, 30):
             st.warning("Waiting for first 15-min candle (till 9:30 AM)... No signals allowed before 9:30.")
@@ -5614,7 +5614,7 @@ elif MENU =="Live Trade":
         else:
             signal = trading_signal_all_conditions(df)
         #st.write("###  Signal")
-        st.write(signal)
+        st.write("Signal-",signal)
         if signal:
             signal_time = df["Datetime"].iloc[-1].time()   # last candle time
             

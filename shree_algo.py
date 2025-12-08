940b4e1f951d0fd527cf0c2cde570bf65674aafb
Abhi Malusare <malusareabhi1@gmail.com>
2025-12-08 23:15:14 +0530

    Update shree_algo.py

--- shree_algo.py
+++ shree_algo.py
@@ -5674,8 +5674,7 @@ elif MENU =="LIVE TRADE 3":
                 nearest_itm = find_nearest_itm_option(kite, spot, option_type)
                 
                 st.success("Nearest ITM Option Found")
-                #
-                st.write(nearest_itm)
+                #                st.write(nearest_itm)
                 nearest_itm1 = pd.DataFrame([nearest_itm])
                 
                 # Display as table

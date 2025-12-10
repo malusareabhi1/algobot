bec7cb461f6b6290f1e4778ac431961ae88d67f2
Abhi Malusare <malusareabhi1@gmail.com>
2025-12-10 11:16:53 +0530

    Update algo_dec_10.py

--- algo_dec_10.py
+++ algo_dec_10.py
@@ -5989,7 +5989,7 @@ elif MENU =="LIVE TRADE 3":
     st.write("start_time", start_time)
     st.write("end_time", end_time)
     st.write("Now Time", now)
-    qty=1
+    qty=75
     # Check 1: Only run if current time is within trading window
     if start_time <= now <= end_time:
             

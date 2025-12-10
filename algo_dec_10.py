708287864052be6cbcab4aa46cf3655f3ad557ff
Abhi Malusare <malusareabhi1@gmail.com>
2025-12-10 12:24:13 +0530

    Update algo_dec_10.py

--- algo_dec_10.py
+++ algo_dec_10.py
@@ -6029,8 +6029,8 @@ elif MENU =="LIVE TRADE 3":
     #import pytz
     tz = pytz.timezone("Asia/Kolkata")
     signal_time = signal_time.replace(tzinfo=tz)
-    st.write("signal_time",signal_time)
-    st.write("Now Time", now)
+    #st.write("signal_time",signal_time)
+    #st.write("Now Time", now)
     #--------------------------------------------------------------------------------
     # Check 1: Only run if current time is within trading window
     if start_time <= now <= end_time:

4debffe130397d8a35902c9ca3989972adf0fdc1
Abhi Malusare <malusareabhi1@gmail.com>
2025-12-11 16:02:35 +0530

    Update algo_dec_10.py

--- algo_dec_10.py
+++ algo_dec_10.py
@@ -6788,7 +6788,7 @@ elif MENU=="Live IV/RANK":
              strike=strike,
              time_to_expiry=time_to_expiry,
              ltp=ltp,
-             r=0.075
+             r=0.085
          )
      
          # Compute IV Rank

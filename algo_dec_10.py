e7ffc7fb7bbbb365344aa65bc4be6b3bdd1e41c0
Abhi Malusare <malusareabhi1@gmail.com>
2025-12-24 13:13:30 +0530

    Update algo_dec_10.py

--- algo_dec_10.py
+++ algo_dec_10.py
@@ -7154,7 +7154,7 @@ elif MENU =="LIVE TRADE 3":
                 df_sig = pd.DataFrame([latest_sig])
                 
                 # Display as table
-                st.table(df_sig)
+                #st.table(df_sig)
         else:
                 st.warning("No signal triggered in any candle yet.")
    

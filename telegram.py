06f0da109fc38ab472a22a0a8bddfa1e24ec2d60
Abhi Malusare <malusareabhi1@gmail.com>
2025-12-30 11:50:40 +0530

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -13,14 +13,18 @@ CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 st.write("BOT_TOKEN:", BOT_TOKEN)
 st.write("CHAT_ID:", CHAT_ID)
 
-def send_telegram_message(message):
+
+
+def send_telegram_alert(message: str):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    response = requests.post(
-        url,
-        json={"chat_id": CHAT_ID, "text": message},
-        timeout=10
-    )
-    return response
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }
+
+    r = requests.post(url, json=payload, timeout=10)
+    return r.status_code == 200
 
 if st.button("Send Test Message"):
     resp = send_telegram_message("Secure Telegram test 🚀")

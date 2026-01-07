9b8bcd003f97d1fd0e3128a206a3db527d595438
Abhi Malusare <malusareabhi1@gmail.com>
2026-01-07 23:07:40 +0530

    Update project_algo_7_jan.py

--- project_algo_7_jan.py
+++ project_algo_7_jan.py
@@ -3367,7 +3367,7 @@ with st.sidebar:
 
     MENU = st.radio(
         "Navigate",
-        ["Home", "Strategies","My Account", "Zerodha Broker API","Strategy Signals","Strategy Multi Signals", "Dashboard","Backtest","Live Trade","Setting","Paper Trade", "Products", "Support","10.10 Strategy","LIVE TRADE 3","Test1","Live IV/RANK","Telegram","Logout"],
+        ["Home", "Strategies","My Account", "Login Zerodha  API","Strategy Signals","Strategy Multi Signals", "Dashboard","Backtest","Live Trade","Setting","Paper Trade", "Products", "Support","10.10 Strategy","LIVE TRADE 3","Test1","Live IV/RANK","Telegram","Logout"],
         index=0,
     )
 
@@ -5747,7 +5747,7 @@ elif MENU == "Strategies":
 
 
 
-elif MENU == "Zerodha Broker API":
+elif MENU == "Login Zerodha  API":
     from kiteconnect import KiteConnect
     st.title("Zerodha Broker Integration")
 

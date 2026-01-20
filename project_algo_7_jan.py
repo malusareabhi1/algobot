1b637e4996bf95eb14457e174aec3646e3295c17
Abhi Malusare <malusareabhi1@gmail.com>
2026-01-20 23:24:23 +0530

    Update project_algo_7_jan.py

--- project_algo_7_jan.py
+++ project_algo_7_jan.py
@@ -9860,7 +9860,7 @@ elif MENU =="LIVE TRADE 3":
         #==================================================================================================== 
         #signal = trading_signal_all_conditions_final(df_plot) 
         #signal = trading_multi2_signal_all_conditions_5min(df_plot)  
-        signals = trading_multi1_signal_all_conditions(df_plot1) 
+        signal = trading_multi1_signal_all_conditions(df_plot1) 
         #st.write("DEBUG signal:", signal)
         #st.write("Type:", type(signal))
 

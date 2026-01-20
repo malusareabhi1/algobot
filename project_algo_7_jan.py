404d2480c72957ec60f67c52563fb8d39241ab53
Abhi Malusare <malusareabhi1@gmail.com>
2026-01-21 00:21:57 +0530

    Update project_algo_7_jan.py

--- project_algo_7_jan.py
+++ project_algo_7_jan.py
@@ -230,7 +230,7 @@ def trading_multi2_signal_all_conditions_5min(
     st.write("📌 Base Low / High:", base_low, base_high,"📌 OR Low / High:", L1, H1)
     #st.write("📌 OR Low / High:", L1, H1)
     st.write("📌 C1 (9:15 Close):", C1,"📌 Latest Close:", day_df_5m.iloc[-1]["Close_^NSEI"])
-    s#t.write("📌 Latest Close:", day_df_5m.iloc[-1]["Close_^NSEI"])
+    #st.write("📌 Latest Close:", day_df_5m.iloc[-1]["Close_^NSEI"])
     debug_table = pd.DataFrame({
     "Parameter": [
         "Base Low",

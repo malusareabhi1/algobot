8f99ec4701eaab11819790ea219a0580c1e2866b
Abhi Malusare <malusareabhi1@gmail.com>
2026-01-22 09:15:44 +0530

    Update project_algo_7_jan.py

--- project_algo_7_jan.py
+++ project_algo_7_jan.py
@@ -212,7 +212,7 @@ def monitor_position_live_with_theta_table(
     expiry_date,
     option_type="CALL"
 ):  
-    st.write("symbol, qty, entry_price,strike,expiry_date,option_type",symbol, qty, entry_price, strike,  expiry_date,  option_type)   
+    #st.write("symbol, qty, entry_price,strike,expiry_date,option_type",symbol, qty, entry_price, strike,  expiry_date,  option_type)   
     import time as tm 
     ist = pytz.timezone("Asia/Kolkata")
     placeholder = st.empty()

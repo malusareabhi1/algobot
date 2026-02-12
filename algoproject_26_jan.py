6ee91598b6f0218a1335bdc675e10c2fc7132f24
Abhi Malusare <malusareabhi1@gmail.com>
2026-02-12 11:29:21 +0530

    Update algoproject_26_jan.py

--- algoproject_26_jan.py
+++ algoproject_26_jan.py
@@ -13404,6 +13404,7 @@ elif MENU =="LIVE TRADE 3":
                                     st.session_state.order_executed=False 
                                     order_status=st.session_state.order_executed
                                     st.write("order_status",order_status)
+                                    st.write("signal time and session time ",signal_time,st.session_state.last_signal_time)
                                     if signal_time != st.session_state.last_signal_time:
                                      st.write("signal time and session time ",signal_time,st.session_state.last_signal_time)
                                      if not st.session_state.order_executed:
